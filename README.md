59c785aadfdccea233a606c4b2341a9bc843efb7
noahehall <noahehall@users.noreply.github.com>
2022-12-17 04:52:34 -0700

    add vault info to readme

--- README.md
+++ README.md
@@ -1,9 +1,59 @@
 # NIRVAI SCRIPTS
 
 - useful scripts
-- readme & info for scripts coming soon
 
 ## TLDR
 
 - many of the scripts rely on shell fns [within this public repo](https://github.com/noahehall/theBookOfNoah/tree/master/linux/bash_cli_fns)
 - you can setup your shell to be l33t like me by [sourcing this file](https://github.com/noahehall/theBookOfNoah/blob/master/linux/_sourceme_.sh) in the parent directory
+
+## script.vault.sh
+
+- actively used for interacting with a tls vault server behind a tls proxy
+- useful for:
+  - verifying vault http endpoints from the CLI
+  - interacting with vault without execing into a container or opening a browser
+- ENV requirements
+  - `export VAULT_ADDR=https://your.vault.addr:and_port`
+  - `export VAULT_TOKEN=your_vault_token`
+    - can be set to an empty string if you dont have one yet
+
+```sh
+# usage
+./script.vault.sh cmd
+
+## cmds
+### e.g. kv-v2
+enable secret secretEngineType
+
+### e.g. approle
+enable approle approleType
+
+### doesnt work, need to find the correct path
+list-secrets
+
+### create a secret-id for roleName
+create approle-secret roleName
+
+### create a new approle roleName with a list of attached policies
+create approle roleName pol1,polX
+
+### get dynamic postgres creds for roleName
+get db creds roleName
+
+### get the secret (kv-v2) at the given path
+get secret secret/poop
+
+### get the status (sys/healthb) of the vault server
+get status
+
+### get credentials for an approle
+get creds roleId secretId
+
+### get the approle role_id for roleName
+get approle id roleName
+
+### get the openapi spec for some path
+help some/path/
+
+```
--- script.vault.sh
+++ script.vault.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# useful for:
-## verifying endpoints before implementing in core
-## interacting with vault without execing into a container or browser
-## all cmds are something like: ./script.vault.sh do this thing with this data
-
 # @see https://curl.se/docs/manpage.html
 # @see https://stedolan.github.io/jq/manual/
 # @see https://developer.hashicorp.com/vault/api-docs/secret/kv/kv-v2
@@ -16,7 +11,7 @@ TOKEN="${VAULT_TOKEN:?VAULT_TOKEN not set: exiting}"
 TOKEN_HEADER="X-Vault-Token: $TOKEN"
 
 vault_curl() {
-  curl -v --url $1
+  curl -v --url $1 ${@:2}
 }
 vault_curl_auth() {
   vault_curl $1 -H "$TOKEN_HEADER"
@@ -26,6 +21,9 @@ vault_list() {
 }
 vault_post_data() {
   vault_curl_auth $2 --data $1
+}
+vault_post_data_no_auth() {
+  vault_curl $2 --data $1 -X POST
 }
 vault_put_data() {
   vault_curl_auth $2 -X PUT --data $1
@@ -77,14 +75,6 @@ enable)
     ;;
   esac
   ;;
-secret)
-  case $2 in
-  get)
-    # eg: secret get secret/foo
-    vault_curl_auth "$ADDR/secret/data/$3" | jq
-    ;;
-  esac
-  ;;
 list-secrets) # doesnt work
   vault_list "$ADDR/secret/data/" | jq
   ;;
@@ -105,6 +95,10 @@ create)
   ;;
 get)
   case $2 in
+  secret)
+    # eg: get secret secret/foo
+    vault_curl_auth "$ADDR/secret/data/$3" | jq
+    ;;
   status)
     # eg get status
     vault_curl "$ADDR/sys/health" | jq
@@ -113,7 +107,7 @@ get)
     # eg get creds roleId secretId
     data=$(data_login $3 $4)
     echo "getting creds for $3 with $data"
-    vault_post_data $data "$ADDR/auth/approle/login" | jq
+    vault_post_data_no_auth $data "$ADDR/auth/approle/login" | jq
     ;;
   approle)
     case $3 in
@@ -129,16 +123,7 @@ get)
 help)
   vault_curl_auth "$ADDR/$2?help=1" | jq
   ;;
-approle)
-  case $2 in
-  login)
-    # eg: approle login roleId secretId
-    echo "maching logging in with $roleId"
-    # vault_post_data "$ADDR/v1/auth/approle/login"
-    ;;
-  esac
-  ;;
 *)
-  echo "$1 == enable-secret|enable-approle"
+  echo -e "invalid request: see root/README.md for help"
   ;;
 esac
