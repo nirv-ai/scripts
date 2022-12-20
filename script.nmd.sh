315d2d0309935c5ecc9bb6c969890ecab97f8c16
noahehall <noahehall@users.noreply.github.com>
2022-12-20 07:27:05 -0700

    update nmd script for prod

--- script.nmd.sh
+++ script.nmd.sh
@@ -1,66 +1,59 @@
 #!/usr/bin/env bash
 
-# you are required to be explicit with cmds in this file
-# use `help` in response to unbound variable at a cmd path
 set -eu
 
 nmd() {
   nomad $@
 }
 
-case $1 in
-status)
+nmdhelp='get|create|team|start'
+nmdcmd=${1:-help}
+
+case $nmdcmd in
+start)
   case $2 in
-  node) nmd node status ;;
-  all) nmd status ;;
-  help) echo -e "node|all" ;;
+  dev)
+    echo -e "starting agents in dev mode"
+    nmd agent -dev -bind 0.0.0.0 -log-level INFO
+    ;;
+  *) echo -e "dev| ..." ;;
   esac
   ;;
-help) echo -e "status | ..." ;;
-esac
-
-nmd_team() {
-  if [ "$1" = "d" ]; then
-    nmd server members -detailed
-  else
-    nmd server members
-  fi
-}
-
-nmd_agent() {
-  case $1 in
-  dev)
-    echo -e "starting dev mode"
-    nomad agent -dev -bind 0.0.0.0 -log-level INFO
-    ;;
-  *) echo -e "dev | ..." ;;
-  esac
-}
-
-# @see https://developer.hashicorp.com/nomad/tutorials/get-started/get-started-jobs
-nmd_plan() {
-  echo -e "creating job plan for $1"
-  nmd job plan $1.nomad
-}
-
-nmd_job() {
-  case $1 in
-  init)
-    if [[ -z $2 ]]; then
-      echo '$2 === filename'
-      return 1
+team)
+  echo -e "retrieving nomad server agents"
+  nmd server members -detailed
+  ;;
+create)
+  case $2 in
+  job)
+    jobname=${3:-""}
+    if [[ -z $jobname ]]; then
+      echo 'syntax: `create job jobName`'
+      exit 1
     fi
-    echo -e "creating new job $2.nomad in the current dir"
-    nmd job init -short $2
+
+    echo -e "creating new job $3.nomad in the current dir"
+    nmd job init -short "$3.nomad"
     ;;
-  run)
-    # todo: check that file exists before running
-    echo -e "running job $2"
-    nmd job run ${2}.nomad
-    ;;
-  s)
-    echo -e "status for job $2:"
-    nmd job status $2
+  *) echo -e "job ..." ;;
+  esac
+  ;;
+get)
+  gethelp='get status|loc|loc-logs|plan'
+  cmdname=${2:-""}
+  if [[ -z $cmdname ]]; then
+    echo -e $gethelp
+    exit 1
+  fi
+
+  case $2 in
+  status)
+    case $3 in
+    node) nmd node status ;;
+    all) nmd status ;;
+    job) nmd job status $4 ;;
+    *) echo -e "node|all|job" ;;
+    esac
     ;;
   loc)
     echo -e "checking allocation for id $2"
@@ -70,6 +63,37 @@ nmd_job() {
     echo -e "fetching task $3 logs for allocation id $2 "
     nmd alloc logs $2 $3
     ;;
-  *) echo -e 'init | run | s | loc | loc-logs ...' ;;
+  plan)
+    jobname=${3:-""}
+    if [[ -z $jobname ]]; then
+      echo 'syntax: `create plan jobName`'
+      exit 1
+    fi
+    if test ! -f "$3.nomad"; then
+      echo -e "ensure jobspec $3.nomad exists in current dir"
+      echo -e 'create a new job plan with `create job jobName`'
+      exit 1
+    fi
+
+    echo -e "creating job plan for $3"
+    nmd job plan "$3.nomad"
+    ;;
+  *) echo -e $gethelp ;;
   esac
-}
+  ;;
+run)
+  jobname=${2:-""}
+  if [[ -z $jobname ]]; then
+    echo 'syntax: `run jobName`'
+    exit 1
+  fi
+  if test ! -f "$2.nomad"; then
+    echo -e "ensure jobspec $2.nomad exists in current dir"
+    echo -e 'create a new job with `create job jobName`'
+    exit 1
+  fi
+  echo -e "running job $2"
+  nmd job run ${2}.nomad
+  ;;
+*) echo -e $nmdhelp ;;
+esac
