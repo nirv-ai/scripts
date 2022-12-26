7820f998355e4ed15fb8514c100afbb329eac38a
noah edward hall <10324554+noahehall@users.noreply.github.com>
2022-12-26 15:07:23 -0500

    Vault: vault.sh finale (#18)
    * update vault scripts for batch processing
    
    * add unseal logic to script
    
    * move stuff into scripts
    
    * vault
    
    * extract retrieval of unseal token into sep fn
    
    * integrate with remaining approle auth endpoints
    
    * reorder enable args to be more intuitive
    
    * rename creds) to approle-creds)
    
    * working through enable_something_in_dir
    
    * setup auto enabling any vault feature with syntax: enable_XXX.thisThing.atThisPAth
    
    * add checks for file sytnax
    
    * enable all features
    
    * config all approle roles
    
    * setup required endpoints for postgres vault integration
    
    * figured out endpoints for token-role and token
    
    * handle kv2 configuration automation
    
    * add todo for removing the vault cli from scripts
    
    * extract postiional args into vars and use that
    
    * clean engine process comments
    
    * update logs
    
    * vault bootstrap complete
    
    Co-authored-by: noahehall <noahehall@users.noreply.github.com>

--- script.vault.sh
+++ script.vault.sh
@@ -17,7 +17,17 @@ AUTH_APPROLE=auth/approle
 AUTH_APPROLE_ROLE=$AUTH_APPROLE/role
 AUTH_TOKEN=auth/token
 AUTH_TOKEN_ACCESSORS=$AUTH_TOKEN/accessors
-TOKEN_CREATE_CHILD=$AUTH_TOKEN/create
+DB=database
+DB_CONFIG=$DB/config                  # LIST this, DELETE this/:name, POST this/:name {connection}
+DB_CREDS=$DB/creds                    # GET this/:name
+DB_RESET=$DB/reset                    # POST this/:name,
+DB_ROLES=$DB/roles                    # LIST this, [GET|DELETE] this/:name, POST this/:name {config},
+DB_ROTATE=$DB/rotate-root             # POST this/:name ,
+DB_STATIC_ROLE=$DB/static-roles       # LIST this, [GET|DELETE] this/:name, POST this/:name {config}
+DB_STATIC_CREDS=$DB/static-creds      # GET this/:name,
+DB_STATIC_ROTATE=$DB/rotate-role      # POST this/:name,
+TOKEN_CREATE_CHILD=$AUTH_TOKEN/create # POST this/:rolename, POST this {config}
+TOKEN_CREATE_ROLE=$AUTH_TOKEN/roles   # POST this/:rolename {config}
 TOKEN_CREATE_ORPHAN=$AUTH_TOKEN/create-orphan
 TOKEN_INFO=$AUTH_TOKEN/lookup
 TOKEN_INFO_ACCESSOR=$AUTH_TOKEN/lookup-accessor
@@ -28,9 +38,19 @@ TOKEN_RENEW_AXOR=$AUTH_TOKEN/renew-accessor
 TOKEN_REVOKE_ID=$AUTH_TOKEN/revoke
 TOKEN_REVOKE_SELF=$AUTH_TOKEN/revoke-self
 TOKEN_REVOKE_AXOR=$AUTH_TOKEN/revoke-accessor
-TOKEN_REVOKE_PARENT=$AUTH_TOKEN/revoke-orphan #children become orphans, parent secrets revoked
-TOKEN_ROLES=$AUTH_TOKEN/roles                 # this/roleId [-X [DELETE | POST]] | this -X LIST
-SECRET_DATA=secret/data
+TOKEN_REVOKE_PARENT=$AUTH_TOKEN/revoke-orphan # children become orphans, parent secrets revoked
+TOKEN_ROLES=$AUTH_TOKEN/roles                 # LIST this, [DELETE|POST] this/:roleId
+# the default kv1 engine is mounted at env
+SECRET_KV1=env # [GET|LIST] this/:path, POST this/:path {json}
+# the default kv2 secret is mounted at secret
+SECRET_KV2=secret
+SECRET_KV2_DATA=$SECRET_KV2/data        # GET this/:path?version=X, PATCH this/:path {json} -H Content-Type application/merge-patch+json, DELETE this/:path
+SECRET_KV2_RM=$SECRET_KV2/delete        # POST this/:path {json}
+SECRET_KV2_RM_UNDO=$SECRET_KV2/undelete # POST this/:path {json}
+SECRET_KV2_ERASE=$SECRET_KV2/destroy    # POST this:path {json}
+SECRET_KV2_CONFIG=$SECRET_KV2/config    # GET this, POST this {config}
+SECRET_KV2_SUBKEYS=$SECRET_KV2/subkeys  # GET this/:path?version=X&depth=Y
+SECRET_KV2_KEYS=$SECRET_KV2/metadata    # [GET|LIST|DELETE] this/:path, POST this/:path {json}, PATCH this/:path {json} -H Content-Type application/merge-patch+json,
 SYS_AUTH=sys/auth
 SYS_HEALTH=sys/health
 SYS_MOUNTS=sys/mounts
@@ -38,8 +58,7 @@ SYS_LEASES=sys/leases
 SYS_LEASES_LOOKUP=$SYS_LEASES/lookup
 SYS_LEASES_LOOKUP_DB_CREDS=$SYS_LEASES_LOOKUP/database/creds
 SYS_POLY=sys/policies
-SYS_POLY_ACL=$SYS_POLY/acl # -X PUT this/policyName
-DB_CREDS=database/creds
+SYS_POLY_ACL=$SYS_POLY/acl # PUT this/:polyName
 
 ######################## ERROR HANDLING
 invalid_request() {
@@ -56,18 +75,32 @@ throw_if_file_doesnt_exist() {
     exit 1
   fi
 }
+throw_if_dir_doesnt_exist() {
+  if test ! -d "$1"; then
+    echo -e "directory doesnt exist: $1"
+    exit 1
+  fi
+}
 get_payload_path() {
   local path=${1:?'cant get unknown path: string not provided'}
 
-  echo "$(pwd)/$path"
+  case $path in
+  "/"*) echo $path ;;
+  *) echo "$(pwd)/$path" ;;
+  esac
 }
-get_payload_filename() {
+get_file_name() {
+  local some_path=${1:?'cant get unknown path: string not provided'}
+  echo "${some_path##*/}"
+}
+get_filename_without_extension() {
   local full_path_with_ext=${1:?'cant get unknown path: string not provided'}
-  local file_with_ext="${full_path_with_ext##*/}"
+  local file_with_ext=$(get_file_name $full_path_with_ext)
 
-  echo "${file_with_ext%.*}" # file without extension
+  # wont work if file.name.contains.periods
+  # will return the `file` in above example
+  echo "${file_with_ext%.*}"
 }
-
 ####################### REQUESTS
 vault_curl() {
   if [ "$DEBUG" = 1 ]; then
@@ -75,7 +108,8 @@ vault_curl() {
     echo -e "[url]: $1\n[args]: ${@:2}\n------------\n\n"
   fi
 
-  curl -v -H "Connection: close" --url $1 "${@:2}" | jq
+  # curl -v should not be used outside of DEV
+  curl -H "Connection: close" --url $1 "${@:2}" | jq
 }
 vault_curl_auth() {
   vault_curl $1 -H "$TOKEN_HEADER" "${@:2}"
@@ -85,6 +119,9 @@ vault_list() {
 }
 vault_post_no_data() {
   vault_curl "$1" -X POST -H "$TOKEN_HEADER"
+}
+vault_delete() {
+  vault_curl "$1" -X DELETE -H "$TOKEN_HEADER"
 }
 vault_post_data_no_auth() {
   vault_curl $2 --data "$1"
@@ -96,7 +133,7 @@ vault_put_data() {
   vault_curl_auth $2 -X PUT --data "$1"
 }
 vault_patch_data() {
-  vault_curl_auth $2 -X PATCH --data "$1"
+  vault_curl_auth $2 -X PATCH -H Content-Type application/merge-patch+json --data "$1"
 }
 
 #################### DATA
@@ -124,6 +161,22 @@ data_axor_only() {
   )
   echo $data
 }
+data_secret_id_only() {
+  local data=$(
+    jq -n -c \
+      --arg secret_id $1 \
+      '{ "secret_id":$secret_id }'
+  )
+  echo $data
+}
+data_secret_id_axor_only() {
+  local data=$(
+    jq -n -c \
+      --arg secret_id_axor $1 \
+      '{ "secret_id_accessor":$secret_id_axor }'
+  )
+  echo $data
+}
 data_login() {
   local data=$(
     jq -n -c \
@@ -140,48 +193,254 @@ data_policy_only() {
     \"policy\": \""
     cat $1 | sed '/^[[:blank:]]*#/d;s/#.*//' | sed 's/\"/\\\"/g' | tr -d '\n' ##  Remove comments and serialize string
     printf "\"
-}"
+    }"
   )
   echo "$data"
 }
-data_policies_only() {
-  # TODO: delete these and use data_policy_only
-  local data=$(
-    jq -n -c \
-      --arg policy $1 \
-      '{
-        "token_policies": [ $policy ],
-        "token_ttl": "24h",
-        "token_max_ttl": "24h"
-        }'
+
+#################### single use fns
+## single executions
+## -n=key-shares
+## -t=key-threshold (# of key shares required to unseal)
+init_vault() {
+  # TODO: we should NOT Be using the vault cli for anything in this file
+  echo -e 'this may take some time...'
+  vault operator init \
+    -format="json" \
+    -n=2 \
+    -t=2 \
+    -root-token-pgp-key="$JAIL/root.asc" \
+    -pgp-keys="$JAIL/root.asc,$JAIL/admin_vault.asc" >$JAIL/root.unseal.json
+}
+get_single_unseal_token() {
+  echo $(
+    cat $JAIL/root.unseal.json |
+      jq -r ".unseal_keys_b64[$1]" |
+      base64 --decode |
+      gpg -dq
   )
-  echo $data
+}
+get_unseal_tokens() {
+  echo -e "VAULT_TOKEN:\n\n$VAULT_TOKEN\n"
+  echo -e "UNSEAL_TOKEN(s):\n"
+  unseal_threshold=$(cat $JAIL/root.unseal.json | jq '.unseal_threshold')
+  i=0
+  while [ $i -lt $unseal_threshold ]; do
+    echo -e "\n$(get_single_unseal_token $i)"
+    i=$((i + 1))
+  done
+}
+unseal_vault() {
+  unseal_threshold=$(cat $JAIL/root.unseal.json | jq '.unseal_threshold')
+  i=0
+  while [ $i -lt $unseal_threshold ]; do
+    vault operator unseal $(get_single_unseal_token $i)
+    i=$((i + 1))
+  done
+}
+create_policy() {
+  syntax='syntax: create_policy [/]path/to/distinct_poly_name.hcl'
+  payload="${1:?$syntax}"
+  payload_path=$(get_payload_path $payload)
+  throw_if_file_doesnt_exist $payload_path
+  payload_data=$(data_policy_only $payload_path)
+  payload_filename=$(get_filename_without_extension $payload_path)
+
+  echo -e "creating policy $payload_filename:\n$(cat $payload_path)"
+  vault_post_data "$payload_data" $ADDR/$SYS_POLY_ACL/$payload_filename
+}
+create_approle() {
+  syntax='syntax: create_approle [/]path/to/distinct_role.json'
+  payload="${1:?$syntax}"
+  payload_path=$(get_payload_path $payload)
+  throw_if_file_doesnt_exist $payload_path
+  payload_filename=$(get_filename_without_extension $payload_path)
+
+  echo -e "creating approle $payload_filename:\n$(cat $payload_path)"
+  vault_post_data "@$payload_path" "$ADDR/$AUTH_APPROLE_ROLE/$payload_filename"
+}
+enable_something() {
+  # syntax: enable_something thisThing atThisPath
+  # eg enable_something kv-v2 secret
+  # eg enable_something approle approle
+  # eg enable_something database database
+  data=$(data_type_only $1)
+  echo -e "\n\nenabling vault feature: $data at path $2"
+
+  case $1 in
+  kv-v1 | kv-v2 | database)
+    URL="$SYS_MOUNTS/$2"
+    ;;
+  approle)
+    URL="$SYS_AUTH/$2"
+    ;;
+  *) invalid_request ;;
+  esac
+  vault_post_data $data "$ADDR/$URL"
+}
+################################ workflows
+process_policies_in_dir() {
+  local policy_dir_full_path="$(pwd)/$1/*"
+  echo -e "\nchecking for policies in: $policy_dir_full_path"
+
+  for file_starts_with_policy_ in $policy_dir_full_path; do
+    case $file_starts_with_policy_ in
+    *"/policy_"*)
+      echo -e "\nprocessing policy: $file_starts_with_policy_\n"
+      create_policy $file_starts_with_policy_
+      ;;
+    esac
+  done
+}
+process_engine_configs() {
+  local engine_config_dir_full_path="$(pwd)/$1/*"
+  echo -e "\nchecking for engine configuration files in: $engine_config_dir_full_path"
+
+  for file_starts_with_secret_ in $engine_config_dir_full_path; do
+    local engine_config_filename=$(get_file_name $file_starts_with_secret_)
+
+    # configure shell to parse filename into expected components
+    PREV_IFS="$IFS"                # save prev boundary
+    IFS="."                        # secret_TYPE.TWO.THREE.FOUR.json
+    set -f                         # stop wildcard * expansion
+    set -- $engine_config_filename # break filename @ '.' into positional args
+
+    # reset shell back to normal
+    set +f
+    IFS=$PREV_IFS
+
+    engine_type=${1:-''}
+    two=${2:-''}
+    three=${3:-''}
+    four=${4:-''}
+
+    case $engine_type in
+    secret_kv2)
+      echo -e "\n$engine_type\n[PATH]: $two\n[TYPE]: $three\n"
+
+      case $3 in
+      config)
+        echo -e "creating config for $engine_type enabled at path: $two"
+        vault_post_data "@${file_starts_with_secret_}" "$ADDR/$two/$three"
+        ;;
+      *) echo -e "ignoring unknown file format: $engine_config_filename" ;;
+      esac
+      ;;
+    secret_database)
+      echo -e "\n$engine_type\n[NAME]: $two\n[CONFIG_TYPE]: $three\n"
+
+      case $three in
+      config)
+        echo -e "creating config for db: $two\n"
+        vault_post_data "@${file_starts_with_secret_}" "$ADDR/$DB_CONFIG/$two"
+        vault_post_no_data "$ADDR/$DB_ROTATE/$two"
+        ;;
+
+      role)
+        echo -e "creating role ${four} for db ${two}\n"
+        vault_post_data "@${file_starts_with_secret_}" "$ADDR/$DB_ROLES/$four"
+        ;;
+      *) echo -e "ignoring file with unknown format: $engine_config_filename" ;;
+      esac
+      # echo -e "\nTODO: not ready for database config: $file_starts_with_secret_\n"
+      ;;
+    *) echo -e "ignoring file with unknown format: $engine_config_filename" ;;
+    esac
+  done
+}
+process_token_role_in_dir() {
+  local token_role_dir_full_path="$(pwd)/$1/*"
+  echo -e "\nchecking for token roles in: $token_role_dir_full_path"
+
+  for file_starts_with_token_role in $token_role_dir_full_path; do
+    case $file_starts_with_token_role in
+    *"/token_role"*)
+      local token_role_filename=$(get_file_name $file_starts_with_token_role)
+
+      # configure shell to parse filename into expected components
+      PREV_IFS="$IFS"             # save prev boundary
+      IFS="."                     # enable.thisThing.atThisPath
+      set -f                      # stop wildcard * expansion
+      set -- $token_role_filename # break filename @ '.' into positional args
+
+      # reset shell back to normal
+      set +f
+      IFS=$PREV_IFS
+
+      # make request if 2 is set, but 3 isnt
+      if test -n ${2:-''} && test -n ${3:-''} && test -z ${4:-''}; then
+        vault_post_data "@${file_starts_with_token_role}" "$ADDR/$TOKEN_CREATE_ROLE/${2}"
+      else
+        echo -e "ignoring file\ndidnt match expectations: $token_role_filename"
+        echo -e 'filename syntax: ^token_role.ROLE_NAME$\n'
+      fi
+      ;;
+    esac
+  done
+}
+process_auths_in_dir() {
+  local auth_dir_full_path="$(pwd)/$1/*"
+  echo -e "\nchecking for auth configs in: $auth_dir_full_path"
+
+  for file_starts_with_auth_ in $auth_dir_full_path; do
+    case $file_starts_with_auth_ in
+    *"/auth_approle_role_"*)
+      echo -e "\nprocessing approle auth config:\n$file_starts_with_auth_\n"
+      create_approle $file_starts_with_auth_
+      ;;
+    esac
+  done
+}
+enable_something_in_dir() {
+  local enable_something_full_dir="$(pwd)/$1/*"
+  echo -e "\nchecking for enable.thisthing.atthispath files in:\n$enable_something_full_dir\n"
+
+  for file_starts_with_enable_X in $enable_something_full_dir; do
+    case $file_starts_with_enable_X in
+    *"/enable"*)
+      local auth_filename=$(get_file_name $file_starts_with_enable_X)
+
+      # configure shell to parse filename into expected components
+      PREV_IFS="$IFS"       # save prev boundary
+      IFS="."               # enable.thisThing.atThisPath
+      set -f                # stop wildcard * expansion
+      set -- $auth_filename # break filename @ '.' into positional args
+
+      # reset shell back to normal
+      set +f
+      IFS=$PREV_IFS
+
+      # make request if 2 and 3 are set, but 4 isnt
+      if test -n ${2:-} && test -n ${3:-''} && test -z ${4:-''}; then
+        enable_something $2 $3
+      else
+        echo -e "ignoring file\ndidnt match expectations: $auth_filename"
+        echo -e 'filename syntax: ^enable.THING.AT_PATH$\n'
+      fi
+      ;;
+    esac
+  done
 }
 
 case $1 in
+init) init_vault ;;
+get_unseal_tokens) get_unseal_tokens ;;
+unseal) unseal_vault ;;
 enable)
   case $2 in
-  secret | approle | database)
-    # eg enable secret kv-v2
-    # eg enable approle approle
-    # eg enable database database
-    data=$(data_type_only $3)
-    echo -e "enabling secret engine: $2 of $data"
-
-    URL=$(
-      [[ "$2" == secret || "$2" == database ]] &&
-        echo "$SYS_MOUNTS/$2" ||
-        echo "$SYS_AUTH/$2"
-    )
-    vault_post_data $data "$ADDR/$URL"
+  kv-v2 | kv-v1 | approle | database)
+    syntax='syntax: enable thisEngine atThisPath'
+    engine=${2:?$syntax}
+    atpath=${3:?$syntax}
+    enable_something $engine $atpath
     ;;
   *) invalid_request ;;
   esac
   ;;
 list)
   case $2 in
+  # @see https://github.com/hashicorp/vault/issues/1115 list only root tokens
   axors)
-    # @see https://github.com/hashicorp/vault/issues/1115 list only root tokens
     echo -e 'listing all tokens'
     vault_list $ADDR/$AUTH_TOKEN_ACCESSORS
     ;;
@@ -193,6 +452,9 @@ list)
     ;;
   approles)
     vault_list "$ADDR/$AUTH_APPROLE_ROLE"
+    ;;
+  approle-axors)
+    vault_list "$ADDR/$AUTH_APPROLE_ROLE/${3:?'syntax: list approleName'}/secret-id"
     ;;
   postgres)
     case $3 in
@@ -226,20 +488,25 @@ create)
     # eg: create approle-secret bff
     echo -e "creating secret-id for approle $3"
 
-    vault_curl_auth "$ADDR/$AUTH_APPROLE_ROLE/$3/secret-id" -X POST
+    vault_post_no_data "$ADDR/$AUTH_APPROLE_ROLE/$3/secret-id" -X POST
     ;;
   approle)
-    # eg: create approle someName role1,role2,role3
-    data=$(data_policies_only $4)
-    echo -e "upserting approle $3 with policies $data"
+    # eg: create approle [/]path/to/distinct_role_name.json
+    payload_path=${3:?'syntax: create approle path/to/distinct_approle_name.json'}
 
-    vault_post_data $data "$ADDR/$AUTH_APPROLE_ROLE/$3"
+    create_approle $payload_path
     ;;
   token)
     syntax='syntax: create token [child|orphan] path/to/payload.json'
     tokentype=${3:-''}
 
     case $tokentype in
+    for-role)
+      rolename="${4:?'syntax: create token for-role roleName'}"
+
+      echo -e "creating token for role: $rolename"
+      vault_post_no_data $ADDR/$TOKEN_CREATE_CHILD/$rolename
+      ;;
     child)
       payload="${4:?$syntax}"
       payload_path=$(get_payload_path $payload)
@@ -253,14 +520,8 @@ create)
     ;;
   poly)
     syntax='syntax: create poly path/to/distinct_poly_name.hcl'
-    payload="${3:?$syntax}"
-    payload_path=$(get_payload_path $payload)
-    throw_if_file_doesnt_exist $payload_path
-    payload_data=$(data_policy_only $payload_path)
-    payload_filename=$(get_payload_filename $payload_path)
-
-    echo -e "creating policy $payload_filename:\n$(cat $payload_path)"
-    vault_post_data "$payload_data" $ADDR/$SYS_POLY_ACL/$payload_filename
+    policy_path=${3:?$syntax}
+    create_policy $policy_path
     ;;
   *) invalid_request ;;
   esac
@@ -316,7 +577,7 @@ get)
     # eg get status
     vault_curl "$ADDR/$SYS_HEALTH"
     ;;
-  creds)
+  approle-creds)
     # eg get creds roleId secretId
     data=$(data_login $3 $4)
     echo -e "getting creds for $3 with $data"
@@ -336,6 +597,24 @@ get)
       echo -e "getting id for approle $4"
 
       vault_curl_auth "$ADDR/$AUTH_APPROLE_ROLE/$4/role-id"
+      ;;
+    secret-id)
+      echo -e "looking up secret-id for approle $4"
+      syntax='get approle secret-id roleName secretId'
+      rolename=${4:?$syntax}
+      secretid=${5:?$syntax}
+      data=$(data_secret_id_only $secretid)
+
+      vault_post_data "${data}" "$ADDR/$AUTH_APPROLE_ROLE/$rolename/secret-id/lookup"
+      ;;
+    secret-id-axor)
+      echo -e "looking up secret-id accesor for approle $4"
+      syntax='get approle secret-id-axor roleName secretIdAccessor'
+      rolename=${4:?$syntax}
+      secretid=${5:?$syntax}
+      data=$(data_secret_id_axor_only $secretid)
+
+      vault_post_data "${data}" "$ADDR/$AUTH_APPROLE_ROLE/$rolename/secret-id-accessor/lookup"
       ;;
     *) invalid_request ;;
     esac
@@ -404,6 +683,25 @@ revoke)
     echo -e "good bye!"
     vault_post_no_data $ADDR/$TOKEN_REVOKE_SELF
     ;;
+  approle-secret-id)
+    echo -e "revoking secret-id for approle $4"
+    syntax='revoke approle-secret-id roleName secretId'
+    rolename=${3:?$syntax}
+    secretid=${4:?$syntax}
+    data=$(data_secret_id_only $secretid)
+
+    vault_post_data "${data}" "$ADDR/$AUTH_APPROLE_ROLE/$rolename/secret-id/destroy"
+    ;;
+  approle-secret-id-axor)
+    echo -e "revoking secret-id accessor for approle $4"
+    syntax='revoke approle-secret-id-axor roleName secretIdAccessor'
+    rolename=${3:?$syntax}
+    secretidAxor=${4:?$syntax}
+    data=$(data_secret_id_axor_only $secretidAxor)
+
+    vault_post_data "${data}" "$ADDR/$AUTH_APPROLE_ROLE/$rolename/secret-id-accessor/destroy"
+    ;;
+  *) invalid_request ;;
   esac
   ;;
 rm)
@@ -412,9 +710,42 @@ rm)
 
   case $rmwhat in
   token-role)
-    # -X auth/token/roles/$id
+    # -X DELETE? auth/token/roles/$id
     echo -e 'delete token role not setup'
     ;;
+  approle-role) vault_delete "$ADDR/$AUTH_APPROLE_ROLE/${id:?'syntax: rm approle roleName'}" ;;
+  esac
+  ;;
+process)
+  processwhat=${2:-''}
+
+  case $processwhat in
+  policy_in_dir)
+    dir=${3:?'syntax: process policy_in_dir path/to/dir'}
+    throw_if_dir_doesnt_exist $dir
+    process_policies_in_dir $dir
+    ;;
+  token_role_in_dir)
+    dir=${3:?'syntax: process token_in_dir path/to/dir'}
+    throw_if_dir_doesnt_exist $dir
+    process_token_role_in_dir $dir
+    ;;
+  auth_in_dir)
+    dir=${3:?'syntax: process auth_in_dir path/to/dir'}
+    throw_if_dir_doesnt_exist $dir
+    process_auths_in_dir $dir
+    ;;
+  enable_feature)
+    dir=${3:?'syntax: process enable_feature path/to/dir'}
+    throw_if_dir_doesnt_exist $dir
+    enable_something_in_dir $dir
+    ;;
+  engine_config)
+    dir=${3:?'syntax: process engine_config path/to/dir'}
+    throw_if_dir_doesnt_exist $dir
+    process_engine_configs $dir
+    ;;
+  *) invalid_request ;;
   esac
   ;;
 *) invalid_request ;;
