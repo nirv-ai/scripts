79483cf787fce679497361b763c2c26ad0a4d8f5
noah edward hall <10324554+noahehall@users.noreply.github.com>
2022-12-30 17:46:35 -0500

    use config dir env var (#32)
    Co-authored-by: noahehall <noahehall@users.noreply.github.com>

--- haproxy/script.haproxy.sh
+++ haproxy/script.haproxy.sh
@@ -4,11 +4,11 @@ set -eu
 
 SERVICE_PREFIX=${SERVICE_PREFIX:-nirvai}
 PROXY_WORK_DIR=${PROXY_WORK_DIR:-/usr/local/etc/haproxy}
-
+HAPROXY_CONFIG_DIR=${HAPROXY_CONFIG_DIR:-./configs}
 cmdhelp='invalid cmd: @see https://github.com/nirv-ai/docs/tree/main/scripts'
 
 conf_validate() {
-  echo $(docker exec --workdir $PROXY_WORK_DIR -it "${SERVICE_PREFIX}_${1}" haproxy -c -f ./haproxy.cfg)
+  echo $(docker exec --workdir $PROXY_WORK_DIR -it "${SERVICE_PREFIX}_${1}" haproxy -c -f "$HAPROXY_CONFIG_DIR")
 }
 
 conf_info() {
@@ -30,15 +30,15 @@ conf)
   action=${2:-''}
   case $action in
   validate)
-    cunt_name=${3-?'syntax: conf validate SERVICE_NAME'}
+    cunt_name=${3:?'syntax: conf validate SERVICE_NAME'}
     conf_validate $cunt_name
     ;;
   info)
-    cunt_name=${3-?'syntax: conf info SERVICE_NAME'}
+    cunt_name=${3:?'syntax: conf info SERVICE_NAME'}
     conf_info $cunt_name
     ;;
   reload)
-    cunt_name=${3-?'syntax: conf reload SERVICE_NAME'}
+    cunt_name=${3:?'syntax: conf reload SERVICE_NAME'}
     conf_reload $cunt_name
     ;;
   *) echo -e $cmdhelp ;;
