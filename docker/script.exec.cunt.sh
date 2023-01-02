96e58d1b22cfda5f8add3894caab821fa7b88980
noah edward hall <10324554+noahehall@users.noreply.github.com>
2023-01-01 22:08:51 -0500

    nomad: switching to consul workstream (#33)
    * fix get token self to use /lookup-self endpoint
    
    * enhance script exec to allow specifying the full container name
    
    * update exec and nmd
    
    Co-authored-by: noahehall <noahehall@users.noreply.github.com>

--- docker/script.exec.cunt.sh
+++ docker/script.exec.cunt.sh
@@ -4,12 +4,20 @@ set -eu
 
 SERVICE_PREFIX=${SERVICE_PREFIX:-nirvai}
 
-if [ "$#" -eq 0 ]; then
-  echo "please provide a docker compose service name"
-else
-  echo "trying to exec with bash"
-  if ! docker exec -it "${SERVICE_PREFIX}_${1}" bash; then
-    echo -e "trying to exec with sh"
-    docker exec -it "${SERVICE_PREFIX}_${1}" sh
+exec_into_container() {
+  cunt_name=${1:?'container name required for docker exec'}
+
+  echo
+  echo "trying to exec with bash: $cunt_name\n"
+  if ! docker exec -it "$cunt_name" bash; then
+    echo
+    echo "trying to exec with sh: $cunt_name\n"
+    docker exec -it "$cunt_name" sh
   fi
-fi
+}
+
+cunt_name=${1:?'syntax cunt containerName | serviceName'}
+case $cunt_name in
+cunt) exec_into_container ${2:?'syntax: cunt containerName'} ;;
+*) exec_into_container ${SERVICE_PREFIX}_${cunt_name} ;;
+esac
