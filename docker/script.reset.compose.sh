c19562a624ff49f3240f793c99581d9256884117
noahehall <noahehall@users.noreply.github.com>
2023-01-03 06:15:53 -0700

    remove comments and use docs repo

--- docker/script.reset.compose.sh
+++ docker/script.reset.compose.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-###########################
-# resets a container & image
-# for something less destructive, use refresh script
-###########################
-
 set -euo pipefail
 
 ENV=${ENV:-development}
