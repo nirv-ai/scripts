847750f81269b09a94fa88a39632d6fb2ba49925
noahehall <noahehall@users.noreply.github.com>
2023-01-03 08:29:52 -0700

    renew anon volumes on script.reset

--- docker/script.reset.compose.sh
+++ docker/script.reset.compose.sh
@@ -17,7 +17,7 @@ down() {
   docker compose down $down_flags
 }
 up() {
-  up_flags='-d --build --force-recreate'
+  up_flags='-d --build --force-recreate --renew-anon-volumes'
   service_name=${1:-''}
   if test -n "$service_name"; then
     up_flags="$up_flags $service_name"
