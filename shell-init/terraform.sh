81f1c7db2f4f39835cdb96c1ee71bb5e541775b1
noahehall <noahehall@users.noreply.github.com>
2023-01-17 19:24:08 -0700

    update tf

--- shell-init/terraform.sh
+++ shell-init/terraform.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 # terraform ----------------------------
-alias tf='terraform'
-
+tf() {
+  terraform "#@"
+}
 tf_reset_aws_env_vars() {
   export TF_VAR_AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID"
   export TF_VAR_AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY"
@@ -10,7 +11,6 @@ tf_reset_aws_env_vars() {
   export TF_VAR_AWS_SESSION_TOKEN="$AWS_SESSION_TOKEN"
   export TF_VAR_AWS_SECURITY_TOKEN="$AWS_SECURITY_TOKEN"
 }
-
 tf_plan() {
   tf init
   echo -e "resetting tf aws vars"
@@ -24,11 +24,9 @@ tf_plan() {
   # if using terraform cloud, ensure its setup to run local
   tf_plan_local
 }
-
 tf_plan_local() {
   terraform plan -out tfplan
 }
-
 tf_plan_destroy() {
   tf init
   echo -e "resetting tf aws vars"
@@ -39,67 +37,55 @@ tf_plan_destroy() {
   tf_validate
   echo -e "generating destroy.tfplan file"
 
-  terraform plan -destroy -out destroy.tfplan
+  tf plan -destroy -out destroy.tfplan
 }
-
 tf_apply() {
   echo -e "resetting tf aws vars"
   tf_reset_aws_env_vars
   if [ ! -z "$1" ]; then
     echo -e 'applying local tfplan'
-    terraform apply
+    tf apply
   else
     echo -e 'applying terrarform cloud tfplan'
-    terraform apply
+    tf apply
   fi
 }
-
 tf_output() {
-  terraform output --json
+  tf output --json
 }
-
 tf_show() {
   echo -e "getting current state of infrastructre"
   # if using terraform cloud: ensure tf is set to run locally
   echo -e "getting current state drift from tfplan"
-  terraform show tfplan
+  tf show tfplan
   echo ""
   echo -e "we mare managing the following resources:"
   tf_statelist
 }
-
 tf_graph() {
-  terraform graph -plan tfplan
+  tf graph -plan tfplan
 }
-
 tf_destroy() {
-  terraform apply destroy.tfplan
+  tf apply destroy.tfplan
 }
-
 tf_fmt() {
-  terraform fmt
+  tf fmt
 }
-
 tf_validate() {
-  terraform validate
+  tf validate
 }
-
 tf_state_list() {
-  terraform state list
+  tf state list
 }
-
 tf_state_pull() {
-  terraform state pull
+  tf state pull
 }
-
 tf_state_rm() {
-  terraform state rm $1
+  tf state rm $1
 }
-
 tf_state_show() {
-  terraform state show
+  tf state show
 }
-
 tf_refresh() {
-  terraform apply -refresh-only
+  tf apply -refresh-only
 }
