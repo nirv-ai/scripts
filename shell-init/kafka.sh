2aff15c39776f7d86e414e4760901e67c0a22641
noahehall <noahehall@users.noreply.github.com>
2023-01-17 19:31:36 -0700

    export fns

--- shell-init/kafka.sh
+++ shell-init/kafka.sh
@@ -11,77 +11,77 @@ KAFKA_DATA_DIR=$KAFKA_DIR/data
 # required by kafka
 ## ^ however 3.2 doesnt require the --zookeeper arg
 zoo_start() {
-    ## make sure you update zookeeper properties when updating kafka versions
-    zookeeper-server-start $KAFKA_DIR/config/zookeeper.properties
+  ## make sure you update zookeeper properties when updating kafka versions
+  zookeeper-server-start $KAFKA_DIR/config/zookeeper.properties
 }
 
 kafka_start() {
-    kafka-server-start $KAFKA_DIR/config/server.properties
+  kafka-server-start $KAFKA_DIR/config/server.properties
 }
 
 kafka_stop() {
-    kafka-server-stop
+  kafka-server-stop
 }
 
 kafka_create_topic() {
-    if [[ $# -eq 1 ]]; then
+  if [[ $# -eq 1 ]]; then
 
-        kafka-topics --create --topic "$1" --bootstrap-server "$KAFKA_BOOTSTRAP_SERVER"
-    else
-        echo "\$1 === topic_name"
-    fi
+    kafka-topics --create --topic "$1" --bootstrap-server "$KAFKA_BOOTSTRAP_SERVER"
+  else
+    echo "\$1 === topic_name"
+  fi
 }
 
 kafka_describe_topic() {
-    if [[ $# -eq 1 ]]; then
-        kafka-topics --describe --topic "$1" --bootstrap-server "$KAFKA_BOOTSTRAP_SERVER"
-    else
-        echo "\$1 === topic_name"
-    fi
+  if [[ $# -eq 1 ]]; then
+    kafka-topics --describe --topic "$1" --bootstrap-server "$KAFKA_BOOTSTRAP_SERVER"
+  else
+    echo "\$1 === topic_name"
+  fi
 }
 
 kafka_list_topics() {
-    kafka-topics --list --bootstrap-server "$KAFKA_BOOTSTRAP_SERVER"
+  kafka-topics --list --bootstrap-server "$KAFKA_BOOTSTRAP_SERVER"
 }
 
 kafka_list_group_ids() {
-    kafka-consumer-groups --list --bootstrap-server "$KAFKA_BOOTSTRAP_SERVER"
+  kafka-consumer-groups --list --bootstrap-server "$KAFKA_BOOTSTRAP_SERVER"
 }
 
 kafka_send() {
-    if [[ $# -eq 1 ]]; then
-        # echo -e "sending\n---\n${@:2}\n---"
-        # echo "to this topic: $1"
-        kafka-console-producer --topic "$1" --bootstrap-server "$KAFKA_BOOTSTRAP_SERVER"
-    else
-        echo "\$1 === topic_name, then paste in events, ctrlD exit"
-    fi
+  if [[ $# -eq 1 ]]; then
+    # echo -e "sending\n---\n${@:2}\n---"
+    # echo "to this topic: $1"
+    kafka-console-producer --topic "$1" --bootstrap-server "$KAFKA_BOOTSTRAP_SERVER"
+  else
+    echo "\$1 === topic_name, then paste in events, ctrlD exit"
+  fi
 }
 
 kafka_list_topic_partitions() {
-    if [[ $# -eq 1 ]]; then
-        kafka-run-class kafka.tools.GetOffsetShell --broker-list "$KAFKA_BOOTSTRAP_SERVER" --topic "$1"
-    else
-        echo "\$1 === topic_name, then paste in events, ctrlD exit"
-    fi
+  if [[ $# -eq 1 ]]; then
+    kafka-run-class kafka.tools.GetOffsetShell --broker-list "$KAFKA_BOOTSTRAP_SERVER" --topic "$1"
+  else
+    echo "\$1 === topic_name, then paste in events, ctrlD exit"
+  fi
 }
 
 kafka_listen_for_topic_events() {
-    if [[ $# -eq 2 ]]; then
-        # echo -e "sending\n---\n${@:2}\n---"
-        # echo "to this topic: $1"
-        kafka-console-consumer --bootstrap-server "$KAFKA_BOOTSTRAP_SERVER" --topic "$1" --offset 20 --partition "$2"
+  if [[ $# -eq 2 ]]; then
+    # echo -e "sending\n---\n${@:2}\n---"
+    # echo "to this topic: $1"
+    kafka-console-consumer --bootstrap-server "$KAFKA_BOOTSTRAP_SERVER" --topic "$1" --offset 20 --partition "$2"
 
-    else
-        echo "\$1 === topic_name, \$2 === partition"
-    fi
+  else
+    echo "\$1 === topic_name, \$2 === partition"
+  fi
 }
 
 kafka_clean() {
-    if [ -z ${KAFKA_DATA_DIR+x} ]; then
-        echo "KAFKA_DATA_DIR is not set; exiting"
-    else
-        echo "removing files $KAFKA_DATA_DIR/{kafka,zookeeper}/*"
-        rm -rf $KAFKA_DATA_DIR/{zookeeper,kafka}/*
-    fi
+  if [ -z ${KAFKA_DATA_DIR+x} ]; then
+    echo "KAFKA_DATA_DIR is not set; exiting"
+  else
+    echo "removing files $KAFKA_DATA_DIR/{kafka,zookeeper}/*"
+    rm -rf $KAFKA_DATA_DIR/{zookeeper,kafka}/*
+  fi
 }
