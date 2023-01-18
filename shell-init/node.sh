746b60775f4484a6e6630dc245f2b1cf46b5d3ba
noahehall <noahehall@users.noreply.github.com>
2023-01-17 19:21:47 -0700

    update node

--- shell-init/node.sh
+++ shell-init/node.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # node ---------------------------------
-alias npmglobals='npm list -g --depth=0'
-alias nvmaliasnode='nvm alias default node'
-alias nvmdefualtsystem='nvm alias default system'
-alias nvminstalled='nvm ls'
-alias nvmlatestinstall='nvm install node --reinstall-packages-from=default --latest-npm'
-alias nvmlatestlts='nvm ls-remote | grep -i latest'
-alias nvmlatestnpm='nvm install-latest-npm'
-alias nvmstop='nvm deactivate' # only for current shell
+alias npm_globals='npm list -g --depth=0'
+alias nvm_alias_node='nvm alias default node'
+alias nvm_defualt_system='nvm alias default system'
+alias nvm_installed='nvm ls'
+alias nvm_latest_install='nvm install node --reinstall-packages-from=default --latest-npm'
+alias nvm_latest_lts='nvm ls-remote | grep -i latest'
+alias nvm_latest_npm='nvm install-latest-npm'
+alias nvm_stop='nvm deactivate' # only for current shell
