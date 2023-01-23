d7e1c20c6d8c66486dce25478e0297ae6ad7bc7b
noahehall <noahehall@users.noreply.github.com>
2023-01-23 06:28:29 -0700

    update registry.sh

--- docker/script.registry.sh
+++ docker/script.registry.sh
@@ -1,68 +1,88 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # TODO: https://github.com/docker/docs/blob/main/registry/deploying.md#restricting-access
 
 set -euo pipefail
 
-# required
-REG_CERTS_DIR_CUNT=${REG_CERTS_DIR_CUNT:-'/etc/ssl/certs'}
+####################### SETUP
+DOCS_URI='https://github.com/nirv-ai/docs/blob/main/docker/README.md'
+SCRIPTS_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]%/}")" &>/dev/null && pwd)"
 
-# optional
-REG_DOMAIN=${REG_DOMAIN:-nirv.ai}
-REG_SUBD=${REG_SUBD:-dev}
+SCRIPTS_DIR_PARENT="$(dirname $SCRIPTS_DIR)"
+
+# PLATFORM UTILS
+for util in $SCRIPTS_DIR/utils/*.sh; do
+  source $util
+done
+
+######################## INTERFACE
+CERTS_DIR_CUNT=${CERTS_DIR_CUNT:-/run/secrets}
+CERTS_DIR_HOST=${CERTS_DIR_HOST:-/etc/ssl/certs}
 REG_HOST_PORT=${REG_HOST_PORT:-5000}
-REG_HOST_NAME=${REG_HOST_NAME:-"${REG_SUBD}.${REG_DOMAIN}"}
-REG_VOL_NAME=${REG_VOL_NAME:-${REG_SUBD}_registry}
-REG_NAME=${REG_NAME:-$REG_VOL_NAME}
+REG_HOSTNAME=${REG_HOSTNAME:-dev.nirv.ai}
+REG_NAME=${REG_NAME:-registry}
+REG_PRVKEY_NAME=${REG_PRVKEY_NAME:-privkey.pem}
+REG_PUBKEY_NAME=${REG_PUBKEY_NAME:-fullchain.pem}
+REG_VOL_NAME=${REG_VOL_NAME:-registry_volume}
 
-REG_FQDN="${REG_HOST_NAME}:${REG_HOST_PORT}"
+REG_FQDN="${REG_HOSTNAME}:${REG_HOST_PORT}"
 
-############################ utility fns
+# add vars that should be printed when NIRV_SCRIPT_DEBUG=1
+declare -A EFFECTIVE_INTERFACE=(
+  [DOCS_URI]=$DOCS_URI
+  [SCRIPTS_DIR_PARENT]=$SCRIPTS_DIR_PARENT
+  [CERTS_DIR_CUNT]=$CERTS_DIR_CUNT
+  [REG_NAME]=$REG_NAME
+  [REG_VOL_NAME]=$REG_VOL_NAME
+  [REG_FQDN]=$REG_FQDN
+)
+
+######################## CREDIT CHECK
+echo_debug_interface
+
+######################## FNS
 
 get_img_fqdn() {
   echo "$REG_FQDN/$1"
 }
 
-volumes() {
-  echo
-  echo "creating volume $REG_VOL_NAME"
+create_reg_volume() {
+  echo_debug "creating volume $REG_VOL_NAME"
   docker volume create $REG_VOL_NAME || true
 }
 
 push_img() {
-  image=${1:?'cant push: image must be a valid identifier'}
-  echo "pushing image: $image"
+  image=${1:?'image must be a valid identifier'}
+  echo_debug "pushing image: $image"
   docker push $image
 }
 ########################### workflow fns
 
 run_reg() {
-  volumes
+  local host_certs="${CERTS_DIR_HOST}/${REG_HOSTNAME}"
+  local reg_pub_key="${host_certs}/${REG_PUBKEY_NAME}"
+  local reg_prv_key="${host_certs}/${REG_PRVKEY_NAME}"
 
-  real_CERTS_DIR_CUNT="$REG_CERTS_DIR_CUNT"
-  if ! test -d "$real_CERTS_DIR_CUNT"; then
-    echo
-    echo "$real_CERTS_DIR_CUNT not found"
-    exit 1
-  fi
+  throw_missing_file $reg_pub_key 400 'public key required for tls'
+  throw_missing_file $reg_prv_key 400 'prvate key required for tls'
+
+  create_reg_volume
 
   portmap="$REG_HOST_PORT:443"
-  CUNT_CERT_PATH=/etc/ssl/certs
-  CUNT_LIVE_CERT_PATH=$CUNT_CERT_PATH/$REG_HOST_NAME
+  CUNT_PUBKEY=$CERTS_DIR_CUNT/$REG_PUBKEY_NAME
+  CUNT_PRVKEY=$CERTS_DIR_CUNT/$REG_PRVKEY_NAME
 
-  echo
-  echo "creating registry: $REG_NAME on $portmap"
-  echo "\tstore: $REG_VOL_NAME"
-  echo "\tcerts: $real_CERTS_DIR_CUNT"
+  echo_info "creating registry\n[NAME] $REG_NAME:$REG_HOST_PORT"
 
   docker run -d -p $portmap \
     --name $REG_NAME \
     --restart unless-stopped \
     -e REGISTRY_HTTP_ADDR=0.0.0.0:443 \
-    -e REGISTRY_HTTP_TLS_CERTIFICATE=$CUNT_LIVE_CERT_PATH/fullchain.pem \
-    -e REGISTRY_HTTP_TLS_KEY=$CUNT_LIVE_CERT_PATH/privkey.pem \
+    -e REGISTRY_HTTP_TLS_CERTIFICATE=$CUNT_PUBKEY \
+    -e REGISTRY_HTTP_TLS_KEY=$CUNT_PRVKEY \
     -v "$REG_VOL_NAME:/var/lib/registry" \
-    -v "$real_CERTS_DIR_CUNT:$CUNT_CERT_PATH" \
+    -v "$reg_pub_key:$CUNT_PUBKEY" \
+    -v "$reg_prv_key:$CUNT_PRVKEY" \
     registry:2
 
   if [ "$?" -ne 0 ]; then
@@ -72,16 +92,7 @@ run_reg() {
 }
 
 reset_reg() {
-  id=$(docker inspect --format="{{.Id}}" $REG_NAME) 2>/dev/null || true
-  if test ! -z $id; then
-    echo 'reviewing disk logs before resetting: this requires sudo'
-    echo "id of previous registry: $id"
-    logfile="/var/lib/docker/containers/$id/$id-json.log"
-    sudo cat $logfile | jq
-  fi
-
-  echo
-  echo "resetting $REG_NAME"
+  echo_debug "resetting $REG_NAME"
   docker stop $REG_NAME || true
   docker container rm $REG_NAME || true
   run_reg
@@ -90,51 +101,43 @@ reset_reg() {
 tag_image() {
   thisImage=${1:?'syntax "tag thisImage:tagName"'}
 
-  echo
-  echo "received image for tagging: $thisImage"
+  echo_debug "received image for tagging: $thisImage"
 
   case $thisImage in
   $REG_FQDN*)
-    echo
-    echo 'image already tagged: pushing image'
+    # echo_info 'consider using docker compose push for pretagged images'
+    echo_debug 'image already tagged: pushing image'
     push_img $thisImage
     ;;
   *)
     taggedImage=$(get_img_fqdn $thisImage)
-    echo "image taged: $taggedImage"
+    echo_debug "image tagged: $taggedImage"
     docker tag $thisImage $taggedImage
     push_img $taggedImage
-    # echo "removing (ignore if fail) original image and keeping new tagged image"
-    # docker image remove -f $thisImage || true
+    # restricting latest images is bit overreaching
+    # docker rmi -f $(docker images --filter="reference=*/*:latest*" -q)
     ;;
   esac
 
-  # docker rmi -f $(docker images --filter="reference=*/*:latest*" -q)
 }
 tag_running_containers() {
-  echo 'tagging & pushing images for running containers:'
-  echo "$(docker ps --format '{{json .}}' | jq '.Image')"
-  # echo $(docker inspect nirvai_web_ui | jq '.[] | {sha: .Image, name: .Name}')
+  echo_debug "tagging & pushing images for running containers\n$(docker ps --format '{{json .}}' | jq '.Image')"
 
   # haha got lucky on this one
   docker ps --format '{{json .}}' | jq '.Image' | while IFS= read -r cunt; do
-    echo
-    echo "processing cunt: $cunt"
+    echo_debug "processing cunt: $cunt"
 
     # xargs  unquotes the cunts name
     tag_image $(echo $cunt | xargs)
   done
 }
 
-cmds='run|reset|rm|tag|tag_running'
+######################## EXECUTE
 cmd=${1:-''}
 case $cmd in
 run) run_reg ;;
 reset) reset_reg ;;
 tag) tag_image $2 ;;
-tag_running) tag_running_containers ;;
-*)
-  echo
-  echo "commands: $cmds"
-  ;;
+tag-running) tag_running_containers ;;
+*) invalid_request ;;
 esac
