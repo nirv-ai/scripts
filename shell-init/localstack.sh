32d4a349e959287743e77ce75465987c64971498
poop <noahedwardhall@gmail.com>
2023-07-18 05:47:46 -0500

    add terraform local

--- shell-init/localstack.sh
+++ shell-init/localstack.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-## @see https://github.com/localstack/awscli-local
-
-# alias awslocal="AWS_ACCESS_KEY_ID=test AWS_SECRET_ACCESS_KEY=test AWS_DEFAULT_REGION=${DEFAULT_REGION:-$AWS_DEFAULT_REGION} aws --endpoint-url=http://${LOCALSTACK_HOST:-localhost}:4566"
-
 awslocal() {
+  # @see https://github.com/localstack/awscli-local
   # TODOs: this doesnt support aws cloudformation package ... cmd. see the docs and test for that cmd to fix it
   # runs in subshell so the exports dont pollute the env
   (
@@ -18,4 +15,10 @@ awslocal() {
 # localstack takes too long to type ;)
 lstack() {
   localstack "$@"
+}
+
+# tflocal takes too long to type ;)
+tfl() {
+  # @see https://docs.localstack.cloud/user-guide/integrations/terraform/
+  tflocal "$@"
 }
