b696b2ce5da9c849bfcc1a60760a53176b3baaf1
noah edward hall <noahedwardhall@gmail.com>
2023-07-18 06:08:29 -0500

    Localstack (#69)
    * add localstack
    
    * convert awslocal copypasta to a fn
    
    * add terraform local
    
    * add samlocal
    
    * add samlocal

--- shell-init/localstack.sh
+++ shell-init/localstack.sh
@@ -1,7 +1,28 @@
 #!/usr/bin/env bash
 
-alias awslocal="AWS_ACCESS_KEY_ID=test AWS_SECRET_ACCESS_KEY=test AWS_DEFAULT_REGION=${DEFAULT_REGION:-$AWS_DEFAULT_REGION} aws --endpoint-url=http://${LOCALSTACK_HOST:-localhost}:4566"
+awsl() {
+  # @see https://github.com/localstack/awscli-local
+  # TODOs: this doesnt support aws cloudformation package ... cmd. see the docs and test for that cmd to fix it
+  # runs in subshell so the exports dont pollute the env
+  (
+    export AWS_ACCESS_KEY_ID=test
+    export AWS_SECRET_ACCESS_KEY=test
+    export AWS_DEFAULT_REGION=${DEFAULT_REGION:-$AWS_DEFAULT_REGION}
+    aws --endpoint-url=http://${LOCALSTACK_HOST:-localhost}:4566 "$@"
+  )
+}
 
 lstack() {
   localstack "$@"
 }
+
+tfl() {
+  # @see https://docs.localstack.cloud/user-guide/integrations/terraform/
+  tflocal "$@"
+}
+
+saml() {
+  # @see https://github.com/localstack/aws-sam-cli-local
+  samlocal "$@"
+}
+
