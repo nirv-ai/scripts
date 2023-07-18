6f1059bebf197f3cae20020257cbb8ae4d8065e2
poop <noahedwardhall@gmail.com>
2023-07-18 05:07:51 -0500

    convert awslocal copypasta to a fn

--- shell-init/localstack.sh
+++ shell-init/localstack.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
-# TODOs:
-## this doesnt support aws cloudformation package ... cmd. see the docs
-alias awslocal="AWS_ACCESS_KEY_ID=test AWS_SECRET_ACCESS_KEY=test AWS_DEFAULT_REGION=${DEFAULT_REGION:-$AWS_DEFAULT_REGION} aws --endpoint-url=http://${LOCALSTACK_HOST:-localhost}:4566"
+## @see https://github.com/localstack/awscli-local
 
-# localstack takes too long to type
+# alias awslocal="AWS_ACCESS_KEY_ID=test AWS_SECRET_ACCESS_KEY=test AWS_DEFAULT_REGION=${DEFAULT_REGION:-$AWS_DEFAULT_REGION} aws --endpoint-url=http://${LOCALSTACK_HOST:-localhost}:4566"
+
+awslocal() {
+  # TODOs: this doesnt support aws cloudformation package ... cmd. see the docs and test for that cmd to fix it
+  # runs in subshell so the exports dont pollute the env
+  (
+    export AWS_ACCESS_KEY_ID=test
+    export AWS_SECRET_ACCESS_KEY=test
+    export AWS_DEFAULT_REGION=${DEFAULT_REGION:-$AWS_DEFAULT_REGION}
+    aws --endpoint-url=http://${LOCALSTACK_HOST:-localhost}:4566 "$@"
+  )
+}
+
+# localstack takes too long to type ;)
 lstack() {
   localstack "$@"
 }
